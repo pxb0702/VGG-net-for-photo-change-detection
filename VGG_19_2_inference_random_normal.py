a2021e36018f16d42d9e81edc5ba28ae94d66340
Pan <xue.bao.pan@intel.com>
2018-04-12 07:46:12 +0800

    3nd commit VGG19-2 still all zero

---
+++ VGG_19_2_inference_random_normal.py
@@ -0,0 +1,115 @@
+from datetime import datetime
+import math
+import time
+import tensorflow as tf 
+
+# this program is to setup a 2-output (Ture / False)
+# purpose is to train / inference photo has been modified or not
+# 2018-4-11 : this program is to use Random_Normal as the parameter initializer
+#             to see if the "all 0" issue is related with initializer or not
+
+class VGG19_2_inference() :
+    def __init__(self, input_op, keep_prob): 
+        self.p = []
+        self.keep_prob = keep_prob
+        # 1st convolution layer -  2 convolution + 1 max pool 
+        self.conv1_1 = self.conv_op(input_op, name="conv1_1", kh = 3, kw = 3, n_out = 64, dh=1, dw = 1, p=self.p )
+        self.conv1_2 = self.conv_op(self.conv1_1, name="conv1_2", kh = 3, kw = 3, n_out = 64, dh=1, dw = 1, p=self.p )
+        self.pool1 = self.mpool_op(self.conv1_2, name = "pool1", kh = 2, kw = 2, dh = 2, dw = 2)
+
+        # 2nd convolution layer  - 2 convolution + 1 max pool,  
+        self.conv2_1 = self.conv_op(self.pool1, name="conv2_1", kh = 3, kw = 3, n_out = 128, dh=1, dw = 1, p=self.p )
+        self.conv2_2 = self.conv_op(self.conv2_1, name="conv2_2", kh = 3, kw = 3, n_out = 128, dh=1, dw = 1, p=self.p )
+        self.pool2 = self.mpool_op(self.conv2_2, name = "pool2", kh = 2, kw = 2, dh = 2, dw = 2)
+
+        # 3rd convolution layer - 4 convolution layer + 1 max pooling 
+        self.conv3_1 = self.conv_op(self.pool2, name="conv3_1", kh = 3, kw = 3, n_out = 256, dh=1, dw = 1, p=self.p )
+        self.conv3_2 = self.conv_op(self.conv3_1, name="conv3_2", kh = 3, kw = 3, n_out = 256, dh=1, dw = 1, p=self.p )
+        self.conv3_3 = self.conv_op(self.conv3_2, name="conv3_3", kh = 3, kw = 3, n_out = 256, dh=1, dw = 1, p=self.p )
+        self.conv3_4 = self.conv_op(self.conv3_3, name="conv3_4", kh = 3, kw = 3, n_out = 256, dh=1, dw = 1, p=self.p )
+        self.pool3 = self.mpool_op(self.conv3_4, name = "pool3", kh = 2, kw = 2, dh = 2, dw = 2)
+        
+        # 4th convolution layer - 4 convolution layer + 1 max pooling 
+        self.conv4_1 = self.conv_op(self.pool3, name="conv4_1", kh = 3, kw = 3, n_out = 512, dh=1, dw = 1, p=self.p )
+        self.conv4_2 = self.conv_op(self.conv4_1, name="conv4_2", kh = 3, kw = 3, n_out = 512, dh=1, dw = 1, p=self.p )
+        self.conv4_3 = self.conv_op(self.conv4_2, name="conv4_3", kh = 3, kw = 3, n_out = 512, dh=1, dw = 1, p=self.p )
+        self.conv4_4 = self.conv_op(self.conv4_3, name="conv4_4", kh = 3, kw = 3, n_out = 512, dh=1, dw = 1, p=self.p )
+        self.pool4 = self.mpool_op(self.conv4_4, name = "pool4", kh = 2, kw = 2, dh = 2, dw = 2) 
+
+        # 5th convolution layer - 4 convolution layer + 1 max pooling 
+        self.conv5_1 = self.conv_op(self.pool4, name="conv5_1", kh = 3, kw = 3, n_out = 512, dh=1, dw = 1, p=self.p )
+        self.conv5_2 = self.conv_op(self.conv5_1, name="conv5_2", kh = 3, kw = 3, n_out = 512, dh=1, dw = 1, p=self.p )
+        self.conv5_3 = self.conv_op(self.conv5_2, name="conv5_3", kh = 3, kw = 3, n_out = 512, dh=1, dw = 1, p=self.p )
+        self.conv5_4 = self.conv_op(self.conv5_3, name="conv5_4", kh = 3, kw = 3, n_out = 512, dh=1, dw = 1, p=self.p )
+        self.pool5 = self.mpool_op(self.conv5_4, name = "pool5", kh = 2, kw = 2, dh = 2, dw = 2) 
+
+        # reshape for Full connectin network
+        shp = self.pool5.get_shape()
+        flattened_shape = shp[1].value * shp[2].value * shp[3].value
+        resh1 = tf.reshape(self.pool5,[-1, flattened_shape], name = "resh1")
+
+        # FC 1
+        self.fc6 = self.fc_op(resh1, name = "fc6", n_out = 4096, p = self.p)
+        self.fc6_drop = tf.nn.dropout(self.fc6, self.keep_prob, name= "fc6_drop")
+        #FC 2
+        self.fc7 = self.fc_op (self.fc6_drop, name = "fc7", n_out=4096, p = self.p)
+        self.fc7_drop = tf.nn.dropout(self.fc7, self.keep_prob, name = "fc7_drop")
+        #FC 3
+        self.fc8 = self.fc_op (self.fc7_drop, name = "fc8", n_out = 2, p=self.p)
+        # Full result output
+        self.softmax = tf.nn.softmax(self.fc8)
+        # predictions
+        self.predictions = tf.argmax(self.softmax, 1)
+
+        
+    def conv_op(self, input_op, name, kh, kw, n_out, dh, dw, p) : 
+        n_in = input_op.get_shape()[-1].value
+
+        with tf.name_scope(name) as scope : 
+            para_shape = [kh, kw, n_in,n_out]
+            init = tf.random_normal(para_shape, stddev = 0.01)
+            kernel = tf.Variable(init)
+#            kernel = tf.get_variable(scope+"w", 
+#                shape = para_shape, 
+#                dtype = tf.float32, initializer= init) 
+            
+            conv = tf.nn.conv2d(input_op, kernel, (1, dh, dw, 1), padding = "SAME")
+            bias_init_val = tf.constant(0.0, shape = [n_out], dtype = tf.float32)
+            biases = tf.Variable(bias_init_val, trainable=True, name = "b")
+
+            z = tf.nn.bias_add(conv, biases)
+            activation = tf.nn.relu(z, name = scope)
+
+            p += [kernel, biases]
+
+            return activation
+
+
+    def fc_op (self, input_op, name, n_out, p) : 
+        n_in = input_op.get_shape()[-1].value
+
+        with tf.name_scope(name) as scope : 
+            para_shape = [n_in,n_out]
+            init = tf.random_normal(para_shape, stddev = 0.01)
+            kernel = tf.Variable(init)
+            
+#            kernel = tf.get_variable(scope+"w", 
+#                shape = para_shape, 
+#                dtype = tf.float32, initializer= tf.random_normal(stddev = 0.01)) 
+            
+            biases = tf.Variable(tf.constant(0.1, shape = [n_out], dtype = tf.float32), trainable=True, name = "b")
+
+            activation = tf.nn.relu_layer(input_op, kernel, biases, name = scope)
+
+            p += [kernel, biases]
+
+            return activation    
+
+    def mpool_op(self,input_op, name, kh, kw, dh, dw) : 
+        return tf.nn.max_pool(input_op, 
+                        ksize = [1,kh,kw,1], 
+                        strides = [1,dh,dw,1], 
+                        padding = 'SAME', 
+                        name = name)
+
+
---
+++ VGG_19_2_train_face.py
@@ -0,0 +1,157 @@
+# this file is to train a NN for recognize my own face. 
+# use VGG 19 
+
+import tensorflow as tf
+import numpy as np
+import cv2
+import os
+import random
+import sys
+from sklearn.model_selection import train_test_split
+import time
+import VGG_19_2_inference_random_normal as VGG
+
+original_photo_path = './VGG for photo/my_face_224'
+modified_photo_path = './VGG for photo/other_face_224'
+size = 224
+test_size = 50
+
+sample_size = 600
+count_number = 0
+
+imgs = []
+labs = []
+
+def readData(path, h=size, w = size) : 
+#    file_list = os.listdir('./')
+    count_number = 0
+    for filename in os.listdir(path):
+    	if filename.endswith ('.bmp') : 
+    		filename = path+'/'+filename
+			
+    		img = cv2.imread(filename)
+    		count_number = count_number + 1
+    		if count_number > sample_size : 
+    			return 
+#			top,bottom,left,right = getPaddingSize(img)
+			
+# to increase the image 
+			
+#			img = cv2.copyMakeBorder(img, top, bottom, left, right, cv2.BORDER_CONSTANT, value=[0,0,0]) 
+#			img = cv2.resize(img, (h,w))
+			
+    		imgs.append(img) 
+    		labs.append(path)
+#            i = i + 1 
+#            if i>sample_size
+#                return
+			
+print('start to read orignal photo ......')			
+readData(original_photo_path)
+print('start to read modified phto  ......')
+readData(modified_photo_path)
+print('image raed finished')
+
+# convert the image to arrange 
+imgs = np.array(imgs)
+labs = np.array([[0,1] if lab == original_photo_path else [1,0] for lab in labs])
+
+# randomly allocate traing set and test set. 
+train_x, test_x, train_y, test_y = train_test_split(imgs, labs, test_size = 0.05, random_state = random.randint(0,100))
+
+# picture total number, picture  h/w/channel 
+train_x = train_x.reshape(train_x.shape[0], size, size, 3)
+test_x = test_x.reshape(test_x.shape[0], size, size, 3)
+
+# convert to float number 
+train_x = train_x.astype('float32')/255.0
+test_x = test_x.astype('float32')/255.0
+
+
+test_x_sized = test_x[0:test_size]
+test_y_sized = test_y[0:test_size]
+
+print("train_size : %s,  test_size : %s  "%(len(train_x), len(test_x)))
+
+# to setup batch， need to redo to randomly train 
+# batch_size = 100
+batch_size = 32
+num_batch = len(train_x) // batch_size
+
+# set the input / expected output. 
+x = tf.placeholder(tf.float32, [None, size, size, 3])
+y_ = tf.placeholder(tf.float32, [None, 2])
+
+# define the prune probility
+keep_prob_5 = tf.placeholder(tf.float32)
+keep_prob_75 = tf.placeholder(tf.float32)
+
+def cnnTrain() : 
+	out = VGG.VGG19_2_inference(x, keep_prob_5)
+	
+	cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits = out.fc8, labels = y_))
+	
+	train_step = tf.train.AdamOptimizer(0.0001).minimize(cross_entropy)
+#    train_step = tf.train.AdamOptimizer(0.0001).minimize(cross_entropy)
+	# to compare the label is equal or not, and get average accuracy, tf.cast to convert. 
+	
+	accuracy = tf.reduce_mean(tf.cast(tf.equal(tf.argmax(out.fc8, 1), tf.argmax(y_, 1)), tf.float32))
+	
+	# save loss and accuracy for tensor board 
+	tf.summary.scalar("loss", cross_entropy)
+	tf.summary.scalar("accuracy", accuracy)
+	merged_summary_op = tf.summary.merge_all() 
+	
+	# save the data 
+	saver = tf.train.Saver() 
+	
+	with tf.Session() as sess : 
+		
+		sess.run(tf.global_variables_initializer())
+		
+		summary_writer = tf.summary.FileWriter('./tmp', graph = tf.get_default_graph())
+		train_start_time = time.time()
+		
+		for n in range(20) :  # iterated 20 times. 
+			# get 128 pictures for each batch. 
+			for i in range(num_batch) : 
+				batch_x = train_x[i*batch_size : (i+1)*batch_size]
+				batch_y = train_y[i*batch_size : (i+1)*batch_size]
+				
+				# start training simultaneiously train 3 avairables, returen 3 
+				time_used_for_one_step = time.time()
+				_, loss, summary = sess.run([train_step, cross_entropy, merged_summary_op], 
+                                        feed_dict = {x:batch_x, y_:batch_y, keep_prob_5 : 0.5})
+				summary_writer.add_summary(summary, n*num_batch+i)
+				# print the loss 
+				time_used_for_one_step = time.time() -  time_used_for_one_step
+				print (" loop = %d total batch num = %d  batch (in loop) No. = %d step = %d   loss = %f step time used = % f" %(n, num_batch,i, n*num_batch+i, loss, time_used_for_one_step))
+				
+				if (n*num_batch+i) % 10 == 0 : 
+					# check the accuracy 
+		#			acc = accuracy.eval({x: test_x_sized, y_: test_y_sized, keep_prob_5 : 1.0})
+					temp_fc8_train = sess.run(out.fc8, {x:batch_x, y_:batch_y, keep_prob_5 : 0.5})
+					temp_loss = sess.run(cross_entropy, {x:batch_x, y_:batch_y, keep_prob_5 : 0.5})
+					temp_fc8_test = sess.run(out.fc8, feed_dict = {x: test_x_sized, y_: test_y_sized, keep_prob_5 : 1.0})
+					acc = sess.run(accuracy, feed_dict = {x: test_x_sized, y_: test_y_sized, keep_prob_5 : 1.0})
+					print (" step = %d   *** accuracy **** = %f "%(n*num_batch+i, acc)) 
+					if acc > 0.95 and n > 2 : 
+						saver.save (sess, './VGG for photo/train_face.model', global_step = n*num_batch+i)
+						print('total time used for training = %f ' %(time.time()- train_start_time))
+						sys.exit(0) 
+		print('accuracy less than 0.90, exited ! ') 
+		
+		
+cnnTrain()
+				
+				
+	
+
+
+
+
+
+
+
+				
+
